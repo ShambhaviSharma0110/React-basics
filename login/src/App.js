12062bda87ec13cf66874ab6c82819d0e0edd1a7
shambhavisharma0110 <shambelieber@gmail.com>
2020-11-11 19:02:29 +0530

    login

---
+++ login/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './App.css';
+import './Login/Login';
+import Login from './Login/Login';
+
+
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
---
+++ login/src/Login/Login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './login.css'
+
+export const Login = () => {
+    return (
+       <div className = "wrapper">
+           <h1>Welcome To ADG Coding Portal!</h1>
+           <p>Please fill in the following details to get started.</p><br></br>       
+           <div className = "username">
+           <label>Username</label><br></br>
+           <input type= "text"
+            placeholder= "Username"/> 
+            </div>
+            <div className = "pass">
+            <label>Password</label><br></br>
+            <input type= "password"
+            placholder = "Password"/>
+            </div>
+        </div>
+    )
+}
+export default Login;
